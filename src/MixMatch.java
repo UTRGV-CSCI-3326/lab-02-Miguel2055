cd00f8888380e645d3d4f9f16520ace5f4aa1989
Miguel2055 <mikey.ibarra10@gmail.com>
2025-02-03 03:05:39 +0000

    first commit

---
+++ src/MixMatch.java
@@ -0,0 +1,18 @@
+public class MixMatch {
+
+	 public static void main(String[] args) {
+
+        String text = "Hello World";
+        char letter = 'M';
+        short shortNumber = 3000;
+        int varInt = 10;
+        long longNumber = 14676477785L;
+        float floatNumber = 4.14f;
+        double doubleNumber = 2.5365645747;
+        boolean isMixMatch = true;
+
+        System.out.println(text + " <-String | " + letter + " <-char | " + shortNumber + " <-short | " + varInt + " <-int | " + 
+          longNumber + " <-long | " + floatNumber + " <-float | " + doubleNumber + " <-double | " + isMixMatch + " <-boolean |" );
+
+     }
+}
